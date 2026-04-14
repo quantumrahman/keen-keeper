c4de662fd0f0094e766bda005574e04a9f77e796
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-14 23:36:43 +0600

    feat: integrate HeroSection in HomePage

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,7 +1,19 @@
+import { Plus } from 'lucide-react'
+
 const HeroSection = () => {
 
     return (
         <>
+            <div className="w-full flex items-center justify-center flex-col text-center gap-4">
+                <h1 className="font-geist text-3xl font-bold text-[#1F2937] md:text-[38px] lg:text-5xl">Friends to keep close in your life</h1>
+                <div className="w-full max-w-[600px]">
+                    <p className="font-geist text-sm font-normal text-[#64748B] md:text-[15px] lg:text-base">Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.</p>
+                </div>
+                <button className="font-geist text-sm font-semibold text-white flex items-center justify-center gap-1 bg-[#244D3F] py-[10px] px-[14px] rounded-sm transform transition-transform duration-300 hover:-translate-y-0.5 mt-4 md:mt-8 md:text-base md:py-3 md:px-4 cursor-pointer">
+                    <Plus></Plus>
+                    Add a Friend
+                </button>
+            </div>
         </>
     );
 };
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,15 @@
+import ContainerLayout from '../../layouts/ContainerLayout.jsx'
+import HeroSection from '../../components/HeroSection/HeroSection.jsx'
+
 const HomePage = () => {
 
     return (
         <>
+            <div className="w-full min-h-[800px] bg-[#F8FAFC] py-10 md:py-16 lg:py-[80px]">
+                <ContainerLayout>
+                    <HeroSection></HeroSection>
+                </ContainerLayout>
+            </div>
         </>
     );
 };
