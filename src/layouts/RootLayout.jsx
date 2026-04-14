a89f2af595e8cfbfd169fd4dea5e7b2ff319254a
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-14 15:42:48 +0600

    feat: add Header, Footer, and Outlet in RootLayout.js file

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,7 +1,17 @@
+import { Outlet } from 'react-router'
+
+import Header from '../components/Header/Header.jsx'
+import Footer from '../components/Footer/Footer.jsx'
+
 const RootLayout = () => {
 
     return (
         <>
+            <div className='w-full min-h-screen flex items-center justify-between flex-col'>
+                <Header></Header>
+                <Outlet></Outlet>
+                <Footer></Footer>
+            </div>
         </>
     );
 };
