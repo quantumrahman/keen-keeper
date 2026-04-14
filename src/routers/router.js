1926b276d811c8923d61e92a1e76f135d7179394
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-14 15:36:44 +0600

    feat: add routers and export the router

---
+++ src/routers/router.js
@@ -0,0 +1,39 @@
+import { createbrowserRouter } from 'react-router';
+
+import RootLayout from '../layouts/RootLayout.jsx'
+import HomePage from '../pages/HomePage/HomePage.jsx'
+import DetailsPage from '../pages/DetailsPage/DetailsPage.jsx'
+import TimelinePage from '../pages/TimelinePage/TimelinePage.jsx'
+import StatsPage from '../pages/StatsPage/StatsPage.jsx'
+import NotFoundPage from '../pages/NotFoundPage/NotFoundPage.jsx'
+
+const router = createbrowserRouter([
+    {
+        path: '/',
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: HomePage
+            },
+            {
+                path: '/firend-details/:details',
+                Component: DetailsPage
+            },
+            {
+                path: '/timeline',
+                Component: TimelinePage
+            },
+            {
+                path: '/stats',
+                Component: StatsPage
+            }
+        ]
+    },
+    {
+        path: '*',
+        Component: NotFoundPage
+    },
+]);
+
+export default router;
