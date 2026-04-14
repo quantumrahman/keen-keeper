aba7ad364aa1b21a5721b05ed0c6cceeea697221
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-14 17:17:15 +0600

    feat: add container

--- src/layouts/ContainerLayout.jsx
+++ src/layouts/ContainerLayout.jsx
@@ -1,7 +1,10 @@
-const ContainerLayout = () => {
+const ContainerLayout = ({ children }) => {
 
     return (
         <>
+            <div className="w-full max-w-[1110px] mx-auto px-5">
+                {children}
+            </div>
         </>
     );
 };
