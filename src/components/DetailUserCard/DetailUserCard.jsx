b9f1e5c78f84ef04297940f6ec82ae6e0cf17275
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-15 21:14:07 +0600

    feat: integrate friend details page and successfully data showing in UI

--- src/components/DetailUserCard/DetailUserCard.jsx
+++ src/components/DetailUserCard/DetailUserCard.jsx
@@ -1,29 +1,29 @@
 import Tag from '../Tag/Tag.jsx'
 import Badge from '../Badge/Badge.jsx'
 
-const DetailUserCard = () => {
+const DetailUserCard = ({ friend }) => {
 
     return (
         <>
             <div className='w-full flex items-center flex-col gap-3 bg-white p-4 border border-[#E9E9E9] rounded-lg md:p-6'>
                 <div className="w-[60px] h-[60px] bg-[#E9E9E9] rounded-full overflow-hidden md:w-20 md:h-20">
-                    {/* <img src={friend?.picture} alt="user profile picture" /> */}
+                    <img src={friend?.picture} alt="user profile picture" />
                 </div>
                 <div className="w-full flex items-center justify-center flex-col gap-2">
                     <div className="w-full text-center">
-                        <h2 className="font-geist text-lg font-semibold text-[#1F2937] md:text-xl">David Kim</h2>
+                        <h2 className="font-geist text-lg font-semibold text-[#1F2937] md:text-xl">{friend?.name}</h2>
                     </div>
                     <div className="w-full flex items-center justify-center flex-col gap-2">
-                        {/* <Badge variant={`${friend?.status}`}>{friend?.status}</Badge> */}
-                        <Badge variant={`overdue`}>Overdue</Badge>
+                        <Badge variant={`${friend?.status}`}>{friend?.status}</Badge>
+                        {/* <Badge variant={`overdue`}>Overdue</Badge> */}
                         <div className='w-full flex items-center justify-center gap-2 flex-wrap'>
-                            <Tag>Work</Tag>
+                            {friend?.tags.map((tag, idx) => <Tag key={idx}>{tag}</Tag>)}
                         </div>
                     </div>
                 </div>
                 <div className='w-full text-center'>
-                    <h2 className='font-geist text-sm font-medium text-[#64748B] italic mb-2 md:text-base'>"Met during madrasa studies. Comes from a family known for Islamic scholarship and community leadership."</h2>
-                    <span className='font-geist text-xs font-normal text-[#64748B] md:text-sm'>Preferred: david.kim@gmail.com</span>
+                    <h2 className='font-geist text-sm font-medium text-[#64748B] italic mb-2 md:text-base'>"{friend?.bio}"</h2>
+                    <span className='font-geist text-xs font-normal text-[#64748B] md:text-sm'>Preferred: {friend?.email}</span>
                 </div>
             </div>
         </>
--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -1,3 +1,5 @@
+import { useLoaderData } from 'react-router';
+
 import ContainerLayout from '../../layouts/ContainerLayout.jsx';
 import PageHeading from '../../components/PageHeading/PageHeading.jsx'
 import DetailUserCard from '../../components/DetailUserCard/DetailUserCard.jsx'
@@ -6,8 +8,13 @@ import DetailStatsCard from '../../components/DetailStatsCard/DetailStatsCard.js
 import DetailRelationCard from '../../components/DetailRelationCard/DetailRelationCard.jsx'
 import DetailQuickCheckCard from '../../components/DetailQuickCheckCard/DetailQuickCheckCard.jsx'
 import DetailsRecentCard from '../../components/DetailsRecentCard/DetailRecentCard.jsx'
+import dateFormatter from '../../utils/dateFormatter.js'
 
 const DetailsPage = () => {
+    const friend = useLoaderData();
+    
+    const {days_since_contact: days, next_due_date: date, goal} = friend;
+    const formatedDate = dateFormatter(date);
 
     return (
         <>
@@ -15,21 +22,21 @@ const DetailsPage = () => {
                 <ContainerLayout>
                     <PageHeading>Friend Details</PageHeading>
                     <div className='w-full flex justify-center gap-3 flex-col md:gap-6 sm:flex-row'>
-                        <div className='w-full h-fit space-y-[14px] sm:w-[40%] md:w-[30%] md:sticky md:top-[24px] md:space-y-4'>
-                            <DetailUserCard></DetailUserCard>
+                        <div className='w-full h-fit space-y-[14px] sm:w-[50%] md:w-[40%] md:sticky md:top-[24px] md:space-y-4 lg:w-[30%]'>
+                            <DetailUserCard friend={friend}></DetailUserCard>
                             <div className='w-full space-y-2'>
                                 <DetailButton buttonType='primary' text='Snooze 2 Weeks' iconType={'snooze'}></DetailButton>
                                 <DetailButton buttonType='primary' text='Archive' iconType={'archive'}></DetailButton>
                                 <DetailButton buttonType='secondary' text='Delete' iconType={'delete'}></DetailButton>
                             </div>
                         </div>
-                        <div className='w-full sm:w-[60%] md:w-[70%] space-y-3 md:space-y-6'>
+                        <div className='w-full sm:w-[50%] space-y-3 md:w-[60%]  md:space-y-6 lg:w-[70%]'>
                             <div className='w-full grid grid-cols-1 gap-3 lg:gap-6 lg:grid-cols-3'>
-                                <DetailStatsCard value={62} text='Days Since Contact'></DetailStatsCard>
-                                <DetailStatsCard value={62} text='Goal (Days)'></DetailStatsCard>
-                                <DetailStatsCard value={62} text='Next Due'></DetailStatsCard>
+                                <DetailStatsCard value={`${days < 10 ? `0${days}` : days}`} text='Days Since Contact'></DetailStatsCard>
+                                <DetailStatsCard value={goal} text='Goal (Days)'></DetailStatsCard>
+                                <DetailStatsCard value={formatedDate} text='Next Due'></DetailStatsCard>
                             </div>
-                            <DetailRelationCard days={30}></DetailRelationCard>
+                            <DetailRelationCard days={`${days < 10 ? `0${days}` : days}`}></DetailRelationCard>
                             <DetailQuickCheckCard></DetailQuickCheckCard>
                             <DetailsRecentCard></DetailsRecentCard>
                         </div>
