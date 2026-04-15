c40ef4289ee4aecee28184951ad77369367383ab
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-15 11:41:22 +0600

    feat: add PageHeading design

--- src/components/PageHeading/PageHeading.jsx
+++ src/components/PageHeading/PageHeading.jsx
@@ -1,7 +1,12 @@
-const PageHeading = () => {
+const PageHeading = ({ children }) => {
 
     return (
         <>
+            <div className="w-full pt-[48px] pb-4 md:pt-[64px] md:pb-[20px] lg:pt-[80px] lg:pb-[24px]">
+                <h1 className="font-geist text-[32px] font-bold text-[#1F2937] md:text-[40px] lg:text-[48px]">
+                    { children }
+                </h1>
+            </div>
         </>
     );
 };
