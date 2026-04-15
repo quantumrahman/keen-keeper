823c9e524d92bcab6c76ed7e479c46d764fd495a
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-15 11:09:28 +0600

    feat: completed useFriends.js hook

--- src/hooks/useFriends.jsx
+++ src/hooks/useFriends.jsx
@@ -1,6 +1,14 @@
-const useFriends = () => {
+import { useContext } from "react";
+import FriendContext from '../context/FriendContext/FriendContext.jsx'
 
-    return <></>
+const useFriends = () => {
+    const context = useContext(FriendContext);
+
+    if (!context) {
+        throw new Error('useFriends must be used within FriendsProvider');
+    };
+
+    return context;
 };
 
 export default useFriends;
