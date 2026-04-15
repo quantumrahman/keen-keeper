214b4bd7a06e9aced9d925cdaad9ec8658cd69ca
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-15 11:30:31 +0600

    feat: integrate FriendPrivder in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { RouterProvider } from 'react-router'
 import './index.css'
 
 import router from './routers/router.js'
+import FriendProvider from './context/FriendContext/FriendsProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}></RouterProvider>
+    <FriendProvider>
+      <RouterProvider router={router}></RouterProvider>
+    </FriendProvider>
   </StrictMode>
 );
