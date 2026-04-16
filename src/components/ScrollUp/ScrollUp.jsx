eabd6a1c64ddbb3926e295d81c9b17709dc1cc93
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-16 19:03:34 +0600

    feat: create scrollUp component for change page user will still there so this component scrollup to page

--- src/components/ScrollUp/ScrollUp.jsx
+++ src/components/ScrollUp/ScrollUp.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router'
+
 const ScrollUp = () => {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: 'smooth'
+        })
+    }, [pathname]);
 
     return null;
 };
