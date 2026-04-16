be2eba2321ecce39fae28b986f5332b4f1b60d0c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-16 08:39:52 +0600

    feat: add dynamic data with props

--- src/components/TimelineCard/TimelineCard.jsx
+++ src/components/TimelineCard/TimelineCard.jsx
@@ -1,7 +1,28 @@
-const TimelineCard = () => {
+import { FiPhoneCall } from "react-icons/fi";
+import { BiMessageDots } from "react-icons/bi";
+import { LuVideo } from "react-icons/lu";
+
+const TimelineCard = ({ data }) => {
 
     return (
         <>
+            <div className="w-full bg-white border border-[#E9E9E9] rounded-sm py-4 px-4 md:py-4 md:px-4 md:rounded-lg">
+                <div className="flex items-center gap-[14px] md:gap-4">
+                    {
+                        data?.status === 'call'
+                        ? <FiPhoneCall className="text-2xl md:text-[27px]" color="#244D3F"></FiPhoneCall>
+                        : data?.status === 'message'
+                        ? <BiMessageDots className="text-2xl md:text-[27px]" color="#244D3F"></BiMessageDots>
+                        : data?.status === 'video'
+                        ? <LuVideo className="text-2xl md:text-[27px]" color="#244D3F"></LuVideo>
+                        : <FiPhoneCall className="text-2xl md:text-[27px]" color="#244D3F"></FiPhoneCall>
+                    }
+                    <div className="space-y-0.5">
+                        <h3 className="font-geist text-lg font-medium text-[#244D3F] md:text-xl"><span className="capitalize">{data?.status}</span> <span className="font-geist text-[15px] font-normal text-[#64748B] md:text-base lg:text-lg">with {data?.name}</span></h3>
+                        <label className="font-geist text-sm font-medium text-[#64748B] md:text-base">{data?.date}</label>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
