caca73eea291c46d3f95af0882fc633d302f661e
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-04-16 19:04:20 +0600

    feat: intergrate scroll up component in root layouts component

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,11 +2,13 @@ import { Outlet } from 'react-router'
 
 import Header from '../components/Header/Header.jsx'
 import Footer from '../components/Footer/Footer.jsx'
+import ScrollUp from '../components/ScrollUp/ScrollUp.jsx';
 
 const RootLayout = () => {
 
     return (
         <>
+            <ScrollUp></ScrollUp>
             <div className='w-full min-h-screen bg-[#F8FAFC] flex items-center justify-between flex-col'>
                 <Header></Header>
                 <Outlet></Outlet>
